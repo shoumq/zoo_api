220d189ca5087de68f8117b8d37f0685bfffd512
Shoumq <lae345@mail.ru>
2023-08-15 12:26:20 +0300

    api v2.3

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -73,7 +73,14 @@ class AuthController extends Controller
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 
-        return $this->respondWithToken($token);
+//        return $this->respondWithToken($token);
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 43200,
+            'user' => auth()->user()
+        ]);
     }
 
 
